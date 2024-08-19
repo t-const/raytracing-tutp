b2cf57543b88d0318a9b582f386bb57b407b9d07
t-const <t-constantin@outlook.com>
2024-08-19 19:16:55 +0000

    checkpoint 10.3

--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -6,7 +6,9 @@
 
 class sphere : public hittable {
   public:
-    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {
+        // TODO: Initialize the material pointer `mat`.
+    }
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
         vec3 oc = center - r.origin();
@@ -32,6 +34,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        rec.mat = mat;
 
         return true;
     }
@@ -39,4 +42,5 @@ class sphere : public hittable {
   private:
     point3 center;
     double radius;
+    shared_ptr<material> mat;
 };
